85795021ee495b2a0bb90bae65b058929232979f
Your Name <you@example.com>
2017-02-10 09:12:20 -0500

    new work

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status, :deposit
 
+  def initialize(name)
+      @name=name
+      @balance= 1000
+      @status="open"
+
+  end
+
+  def deposit(num)
+    @balance += num
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status = open
+  end
 end
