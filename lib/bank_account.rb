f117481f49998fc9702166173ff21124f0c0ff62
JGBrown <j.grant.brown@gmail.com>
2017-05-08 20:41:52 -0400

    almost there

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,23 +1,31 @@
 class BankAccount
-  attr_reader :name
-  attr_accessor :balance, :status, :deposit
 
+attr_accessor  :balance, :status
+attr_reader :name
   def initialize(name)
-      @name=name
-      @balance= 1000
-      @status="open"
-
+    @name=name
+    @balance= 1000
+    @status="open"
   end
 
-  def deposit(num)
-    @balance += num
+  def deposit(deposit_amount)
+    @balance+=deposit_amount
   end
 
   def display_balance
-    "Your balance is $#{@balance}."
+    "Your balance is $#{self.balance}."
   end
 
   def valid?
-    if @status = open
+    if self.balance > 0 && self.status == "open"
+      true
+    else
+      false
+    end
   end
+
+  def close_account
+    self.status= "closed"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
+require "pry"
+
 class Transfer
+
+  attr_accessor :amount, :sender, :receiver, :status
+
+  def initialize(sender, receiver, amount)
+    @sender= sender
+    @receiver= receiver
+    @amount = amount
+    @status ="pending"
+  end
+
+  def valid?
+    if (self.receiver.valid? == true) && (self.sender.valid? == true)
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+      if self.valid? == true && sender.balance > amount && self.status == "pending"
+        receiver.balance += amount
+        sender.balance -= amount
+        self.status="complete"
+      else
+        self.status="rejected"
+         "Transaction rejected. Please check your account balance."
+
+      end
+  end
   # your code here
 end
